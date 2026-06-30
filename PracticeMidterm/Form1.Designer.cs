1a10a95df9a5b9566e1224877e20335ad69d6a48
Adamskiee <23-61030@g.batstate-u.edu.ph>
2026-06-30 11:52:45 +0800

    Midterm practice with load, delete, and sort users.

---
+++ PracticeMidterm/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PracticeMidterm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(149, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 325);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(265, 363);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(103, 52);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(414, 363);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 52);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(556, 363);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(103, 52);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ PracticeMidterm/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace PracticeMidterm
+{
+    public partial class Form1 : Form
+    {
+        private List<User> users;
+        public Form1()
+        {
+            InitializeComponent();
+            // Upon start, the form renders the users data.
+            loadUsers();
+        }
+
+        private void loadUsers()
+        {
+            string data = File.ReadAllText("../../Data/users.json");
+            users = JsonConvert.DeserializeObject<List<User>>(data);
+            dataGridView1.DataSource = users;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // The show button resets the datas in DataView for demo purposes
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            loadUsers();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            /*
+             * The use of `dataGridView1.SelectedRows` is to identify
+             * what are the rows does the user select.
+             */
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("There is no user selected.");
+                return;
+            }
+
+            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+            {
+                User user = (User)dataGridView1.SelectedRows[i].DataBoundItem;
+                users.Remove(user);
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = users;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            List<User> sortedUsers = users.OrderBy(user => user.name).ToList();
+            dataGridView1.DataSource = sortedUsers;
+            users = sortedUsers;
+        }
+    }
+}
